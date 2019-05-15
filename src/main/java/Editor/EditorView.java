d5098bdbc448d79bd276ebc620c7beabda070612
Romain Baguet <romain.baguet@epita.fr>
2019-05-15 19:30:48 +0200

    [EDITOR] toolbar added

--- src/main/java/Editor/EditorView.java
+++ src/main/java/Editor/EditorView.java
@@ -1,6 +1,7 @@
 package Editor;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,14 +9,76 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 
-public class EditorView {
+public class EditorView extends JFrame {
+    JMenuItem file_new;
+    JMenuItem file_open;
+    JMenuItem file_save;
+    JMenuItem file_exit;
+    JMenuItem edit_undo;
+    JMenuItem edit_redo;
+    JButton newButton;
+    JButton openButton;
+    JButton saveButton;
+    JButton moveButton;
+    JButton selectButton;
+    JButton removeButton;
+    JButton undoButton;
+    JButton redoButton;
+
     public EditorView() {
         //Creating Frame
         JFrame frame = new JFrame("My RPG Maker");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
+        frame.setSize(600, 600);
 
         //Creating Menu bar
+        JMenuBar menuBar = create_menu_bar();
+        //Creating Tool bar
+        JToolBar toolBar = create_tool_bar();
+
+        //Adding every component to the frame
+        frame.setJMenuBar(menuBar);
+        frame.add(toolBar, BorderLayout.NORTH);
+        frame.setVisible(true);
+    }
+
+    private JToolBar create_tool_bar() {
+        JToolBar toolBar = new JToolBar();
+        toolBar.setFloatable(false);
+        ImageIcon new_I = get_icon("new.png");
+        newButton = new JButton(new_I);
+        toolBar.add(newButton);
+        ImageIcon open_I = get_icon("open.png");
+        openButton = new JButton(open_I);
+        toolBar.add(openButton);
+        ImageIcon save_i = get_icon("save.png");
+        saveButton = new JButton(save_i);
+        toolBar.add(saveButton);
+        ImageIcon move_I = get_icon("move.png");
+        moveButton = new JButton(move_I);
+        toolBar.add(moveButton);
+        ImageIcon select_I = get_icon("select.png");
+        selectButton = new JButton(select_I);
+        toolBar.add(selectButton);
+        ImageIcon remove_I = get_icon("remove.png");
+        removeButton = new JButton(remove_I);
+        toolBar.add(removeButton);
+        ImageIcon undo_I = get_icon("undo.png");
+        undoButton = new JButton(undo_I);
+        toolBar.add(undoButton);
+        ImageIcon redo_I = get_icon("redo.png");
+        redoButton = new JButton(redo_I);
+        toolBar.add(redoButton);
+
+        return toolBar;
+    }
+
+    private ImageIcon get_icon(String file) {
+        return new ImageIcon(new ImageIcon(System.getProperty("user.dir") + "/resources/icons/" + file)
+                .getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH));
+    }
+
+    private JMenuBar create_menu_bar() {
         JMenuBar menuBar = new JMenuBar();
         JMenu file = new JMenu("File");
         JMenu edit = new JMenu("Edit");
@@ -24,66 +87,40 @@ public class EditorView {
 
         //Creating Menu Items
         //NEW//
-        JMenuItem file_new = new JMenuItem("new");
+        file_new = new JMenuItem("new");
         file_new.setMnemonic(KeyEvent.VK_M);
         file_new.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, Event.CTRL_MASK, true));
         file_new.setEnabled(true);
         file.add(file_new);
         //OPEN//
-        JMenuItem file_open = new JMenuItem("open");
-        file_open.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-
-                int returnValue = jfc.showOpenDialog(null);
-
-
-                if (returnValue == JFileChooser.APPROVE_OPTION) {
-                    File selectedFile = jfc.getSelectedFile();
-                    String selectFilepath = selectedFile.getAbsolutePath();
-                }
-            }
-        });
+        file_open = new JMenuItem("open");
         file_open.setMnemonic(KeyEvent.VK_M);
         file_open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, Event.CTRL_MASK, true));
         file_open.setEnabled(true);
         file.add(file_open);
         //SAVE//
-        JMenuItem file_save = new JMenuItem("save");
+        file_save = new JMenuItem("save");
         file_save.setMnemonic(KeyEvent.VK_M);
         file_save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, Event.CTRL_MASK, true));
         file_save.setEnabled(true);
         file.add(file_save);
         //EXIT//
-        JMenuItem file_exit = new JMenuItem("exit");
-        file_exit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                frame.dispose();
-            }
-        });
+        file_exit = new JMenuItem("exit");
         file_exit.setEnabled(true);
         file.add(file_exit);
         //UNDO//
-        JMenuItem edit_undo = new JMenuItem("undo");
+        edit_undo = new JMenuItem("undo");
         edit_undo.setMnemonic(KeyEvent.VK_M);
         edit_undo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, Event.CTRL_MASK, true));
         edit_undo.setEnabled(true);
         edit.add(edit_undo);
         //REDO//
-        JMenuItem edit_redo = new JMenuItem("redo");
+        edit_redo = new JMenuItem("redo");
         edit_redo.setMnemonic(KeyEvent.VK_M);
         edit_redo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U, Event.CTRL_MASK, true));
         edit_redo.setEnabled(true);
         edit.add(edit_redo);
 
-        //Misc
-        JButton button1 = new JButton("testButton");
-
-        //Adding every component to the frame
-        frame.getContentPane().add(BorderLayout.NORTH, menuBar);
-        frame.getContentPane().add(BorderLayout.WEST, button1);
-        frame.setVisible(true);
+        return menuBar;
     }
 }
