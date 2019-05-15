d6c1efca98d1ade396bb7c7cbdb9c163def28728
Rayane Amrouche <rayane.amrouche@epita.fr>
2019-05-15 16:39:26 +0200

    [ENGINE][PLAYER] class character created

---
+++ src/main/java/Engine/Player/Character.java
@@ -0,0 +1,23 @@
+package Engine.Player;
+
+public class Character {
+    public Character(String name, String sprite)
+    {
+        
+    }
+
+    //ids
+    private Integer id_;
+    private String name_;
+
+    //sprite
+    private String sprite_;
+
+    //map
+    private Integer pos_x_;
+    private Integer pos_y_;
+
+    //state
+    private Integer state_;
+    private Integer cur_dir_;
+}
