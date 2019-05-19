66e3a0e2b759c21d3f3b7498c8a24bc5bdf28ce0
Amine Heffar <amine.heffar@epita.fr>
2019-05-19 16:25:22 +0200

    [ENGINE][MAP][TILE] adding attribute to know if a tile is walkable and it's images

--- src/main/java/Engine/Map/Tile.java
+++ src/main/java/Engine/Map/Tile.java
@@ -5,10 +5,16 @@ import Engine.Position;
 public class Tile {
     private Position p_;
     private TileType t_;
-    public Tile(int x, int y, TileType t)
+    private Boolean is_Walkable_;
+    private String foreGround_img_;
+    private String backGround_img_;
+
+    public Tile(int x, int y, TileType t, String b, String f)
     {
         p_ = new Position(x, y);
         t_ = t;
+        backGround_img_ = b;
+        foreGround_img_ = f;
     }
     /**
      * @return the t_
@@ -22,5 +28,29 @@ public class Tile {
      */
     public Position getP_() {
         return p_;
+    }
+
+    public Boolean getIs_Walkable_() {
+        return is_Walkable_;
+    }
+
+    public void setIs_Walkable_(Boolean is_Walkable_) {
+        this.is_Walkable_ = is_Walkable_;
+    }
+
+    public String getForeGround_img_() {
+        return foreGround_img_;
+    }
+
+    public void setForeGround_img_(String foreGround_img_) {
+        this.foreGround_img_ = foreGround_img_;
+    }
+
+    public String getBackGround_img_() {
+        return backGround_img_;
+    }
+
+    public void setBackGround_img_(String backGround_img_) {
+        this.backGround_img_ = backGround_img_;
     }
 }
