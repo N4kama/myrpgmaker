4a0e594b3bbcd087de060bf9f324da991021f903
Amine Heffar <amine.heffar@epita.fr>
2019-05-19 17:18:45 +0200

    [ENGINE][MAP][OBJECTS] object class to handle foreground objects

---
+++ src/main/java/Engine/Map/Object.java
@@ -0,0 +1,31 @@
+package Engine.Map;
+
+import Engine.Position;
+
+import java.util.List;
+
+public class Object {
+    private String object_img_;
+    private Position p_;
+
+    public Object(String object_img_, Position p_) {
+        this.object_img_ = object_img_;
+        this.p_ = p_;
+    }
+
+    public String getObject_img_() {
+        return object_img_;
+    }
+
+    public void setObject_img_(String object_img_) {
+        this.object_img_ = object_img_;
+    }
+
+    public Position getP_() {
+        return p_;
+    }
+
+    public void setP_(Position p_) {
+        this.p_ = p_;
+    }
+}
