fb35f8e486f85d470c59c4da8812f22d015267e1
rayane.amrouche <rayane.amrouche@epita.fr>
2019-05-21 21:32:26 +0200

    [ENGINE][EVENT] added variable to event

---
+++ src/main/java/Engine/Vars.java
@@ -0,0 +1,57 @@
+package Engine;
+
+public class Vars {
+
+    public Vars(String lab, Integer val, Integer id_)
+    {
+        label = lab;
+        value = val;
+        id = id_;
+    }
+    private String label;
+    private Integer value;
+    private Integer id;
+
+    /**
+     * @return String return the label
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * @param label the label to set
+     */
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    /**
+     * @return Integer return the value
+     */
+    public Integer getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    /**
+     * @return Integer return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
