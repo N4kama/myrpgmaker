be4ea2f017725e033d81f699670ee0d4670dae54
Zachary Fakir <zachary.fakir@epita.fr>
2019-05-30 18:58:12 +0200

    [EDITOR] FileExplorer implem started

--- src/main/java/FileExplorerPannel/FileExplorerView.java
+++ src/main/java/FileExplorerPannel/FileExplorerView.java
@@ -2,12 +2,31 @@ package FileExplorerPannel;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Observable;
 import java.util.Observer;
 
+import static Utils.WorldTools.loadWorld;
+
 public class FileExplorerView extends JTree implements Observer {
 
-    public FileExplorerView() {
+
+
+    private void generateTree(DefaultMutableTreeNode TreeNode, String path) {
+        File[] files = this.getListFiles(path);
+        for (File file : files) {
+            if (file.isDirectory()) {
+                DefaultMutableTreeNode subDirectory = new DefaultMutableTreeNode(file.getName());
+                generateTree(subDirectory, file.getAbsolutePath());
+                TreeNode.add(subDirectory);
+            } else if (checkWrld(file.getName())) {
+                TreeNode.add(new DefaultMutableTreeNode(file.getName()));
+            }
+
+
+        }
     }
 
     @Override
