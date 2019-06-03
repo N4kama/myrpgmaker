e86ab1c7c5dbd1b02a2f2535a7970b3702f7ec05
Romain Baguet <romain.baguet@epita.fr>
2019-06-03 20:21:21 +0200

    [UTILS] new variable handling player first spawn

--- src/main/java/Utils/SpriteTools.java
+++ src/main/java/Utils/SpriteTools.java
@@ -17,6 +17,7 @@ public class SpriteTools {
 
     public static HashMap<BufferedImage, String> imgToPath = new HashMap<>();
     public static HashMap<String, BufferedImage> pathToImg = new HashMap<>();
+    public static boolean playerModelSelected = false;
     public static boolean is_background = true;
     public static String selectedSprite = null;
     public static MousePointerState mousePointerState = MousePointerState.PLACE;
