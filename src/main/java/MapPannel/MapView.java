473e24cc555a5da9290d3bc3825b315ff24021fd
Amine Heffar <amine.heffar@epita.fr>
2019-06-04 21:17:28 +0200

    [MAPVIEW] making function public and chage it because game map view no more depend on it

--- src/main/java/MapPannel/MapView.java
+++ src/main/java/MapPannel/MapView.java
@@ -37,19 +37,14 @@ public class MapView extends JPanel implements Observer {
         drayObjects(g);
     }
 
-    private void drayObjects(Graphics g) {
+    public void drayObjects(Graphics g) {
         for (EngineObj obj : mapModel.getObjects()) {
             if (!obj.getIs_player()) {
                 BufferedImage img = SpriteTools.openObject(obj.getSprite_());
                 g.drawImage(img, obj.get_y() - img.getHeight() / 2, obj.get_x() - img.getWidth() / 2, null);
-                System.out.println("NOP");
             } else {
-                spritePlayer = SpriteTools.openObject(obj.getSprite_());
-                SpriteTools.setSpriteMove(spritePlayer, walkingLeft, "left");
-                SpriteTools.setSpriteMove(spritePlayer, walkingRight, "right");
-                SpriteTools.setSpriteMove(spritePlayer, walkingUp, "up");
-                SpriteTools.setSpriteMove(spritePlayer, walkingDown, "down");
-                g.drawImage(walkingDown.get(1), obj.get_y() - walkingLeft.get(0).getHeight() / 2, obj.get_x() - walkingLeft.get(0).getWidth() / 2, null);
+                BufferedImage img = SpriteTools.openObject(obj.getSprite_());
+                g.drawImage(img, obj.get_y() - img.getHeight() / 2, obj.get_x() - img.getWidth() / 2, null);
             }
         }
     }
@@ -127,4 +122,5 @@ public class MapView extends JPanel implements Observer {
         BufferedImage img = SpriteTools.openObject(obj.getSprite_());
         g.drawImage(img, obj.get_x() - img.getWidth() / 2, obj.get_y() - img.getHeight() / 2, null);
     }
+
 }
