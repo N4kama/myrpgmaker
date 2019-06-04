d9018da645554d40d9d3c2171e54d7e42643fad3
Amine Heffar <amine.heffar@epita.fr>
2019-06-04 19:26:32 +0200

    [UTILS] add func to get specefic field from sprite

--- src/main/java/Utils/SpriteTools.java
+++ src/main/java/Utils/SpriteTools.java
@@ -4,10 +4,11 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SpriteTools {
-
+    private static final int TILE_SIZE = 32;
     public enum MousePointerState {
         PLACE,
         MOVE,
@@ -49,6 +50,29 @@ public class SpriteTools {
         } catch (IOException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public static void setSpriteMove(BufferedImage img, ArrayList<BufferedImage> arr, String move) {
+        if (move.equals("left")) {
+            arr.add(img.getSubimage(0 * TILE_SIZE, 1 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+            arr.add(img.getSubimage(1 * TILE_SIZE, 1 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+            arr.add(img.getSubimage(2 * TILE_SIZE, 1 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+        }
+        else if (move.equals("right")) {
+            arr.add(img.getSubimage(0 * TILE_SIZE, 2 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+            arr.add(img.getSubimage(1 * TILE_SIZE, 2 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+            arr.add(img.getSubimage(2 * TILE_SIZE, 2 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+        }
+        else if (move.equals("up")) {
+            arr.add(img.getSubimage(0 * TILE_SIZE, 3 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+            arr.add(img.getSubimage(1 * TILE_SIZE, 3 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+            arr.add(img.getSubimage(2 * TILE_SIZE, 3 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+        }
+        else if (move.equals("down")) {
+            arr.add(img.getSubimage(0 * TILE_SIZE, 0 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+            arr.add(img.getSubimage(1 * TILE_SIZE, 0 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+            arr.add(img.getSubimage(2 * TILE_SIZE, 0 * TILE_SIZE, TILE_SIZE, TILE_SIZE));
         }
     }
 }
