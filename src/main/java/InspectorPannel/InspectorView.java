345b40e2067d20449683113e5e9c6272bf8f8f9f
Zachary Fakir <zachary.fakir@epita.fr>
2019-06-05 18:15:24 +0200

    [INSPECTOR] InspectorView updated to modify a Engineobj

--- src/main/java/InspectorPannel/InspectorView.java
+++ src/main/java/InspectorPannel/InspectorView.java
@@ -7,27 +7,52 @@ import java.awt.event.ActionListener;
 
 
 public class InspectorView extends JPanel implements ActionListener {
-    JTextField txtdata;
+    JLabel namet;
+    JLabel typet;
+    JLabel post;
+    JLabel old;
+    JTextField dialog;
     JButton calbtn = new JButton("Save new dialog !");
     public InspectorModel model;
 
     public InspectorView(InspectorModel model) {
         this.model = model;
-        JLabel namet = new JLabel("Name: Zacky");
-        JLabel typet = new JLabel("Type: NPC");
-        JLabel post = new JLabel("Position: 10.5   8.5");
+        if (InspectorModel.obj != null) {
+            namet = new JLabel("Name: " + InspectorModel.obj.getName_());
+
+            if (!InspectorModel.obj.getIs_player())
+                typet = new JLabel("Type: NPC");
+            else
+                typet = new JLabel("Type: Player");
+            post = new JLabel("Position: " + InspectorModel.obj.getPosition_());
+            if (InspectorModel.obj.getDialog() != null)
+                old = new JLabel("Current dialog: " + InspectorModel.obj.getDialog());
+            else
+                old = new JLabel("Current dialog: ");
+        }
+        else
+        {
+            namet = new JLabel("Name: ");
+            typet = new JLabel("Type: ");
+            post = new JLabel("Position: ");
+            old = new JLabel("Current dialog: ");
+        }
+
+
         JLabel label = new JLabel("Set the dialog parameters:", SwingConstants.LEFT);
-        label.setFont(new Font("Courier New", Font.BOLD, 16));
-        txtdata = new JTextField(30);
+        dialog = new JTextField(30);
+
         add(namet);
         add(Box.createRigidArea(new Dimension(1, 5)));
         add(typet);
         add(Box.createRigidArea(new Dimension(1, 5)));
         add(post);
         add(Box.createRigidArea(new Dimension(1, 5)));
+        add(old);
+        add(Box.createRigidArea(new Dimension(1, 5)));
         add(label);
         add(Box.createRigidArea(new Dimension(1, 5)));
-        add(txtdata);
+        add(dialog);
         add(calbtn);
         calbtn.addActionListener(this);
         BoxLayout boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
@@ -37,8 +62,9 @@ public class InspectorView extends JPanel implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == calbtn) {
-            String data = txtdata.getText();
-            System.out.println(data);
+            String data = dialog.getText();
+            if (InspectorModel.obj != null)
+                InspectorModel.obj.setDialog(data);
         }
     }
 }
