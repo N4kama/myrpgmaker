354babbda2a978d2e872dddf89a7177cf81516ee
Zachary Fakir <zachary.fakir@epita.fr>
2019-06-05 17:31:47 +0200

    [INSPECTOR] Inspect view implemented

--- src/main/java/InspectorPannel/InspectorView.java
+++ src/main/java/InspectorPannel/InspectorView.java
@@ -1,11 +1,44 @@
 package InspectorPannel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class InspectorView extends JPanel {
+
+public class InspectorView extends JPanel implements ActionListener {
+    JTextField txtdata;
+    JButton calbtn = new JButton("Save new dialog !");
     public InspectorModel model;
 
     public InspectorView(InspectorModel model) {
         this.model = model;
+        JLabel namet = new JLabel("Name: Zacky");
+        JLabel typet = new JLabel("Type: NPC");
+        JLabel post = new JLabel("Position: 10.5   8.5");
+        JLabel label = new JLabel("Set the dialog parameters:", SwingConstants.LEFT);
+        label.setFont(new Font("Courier New", Font.BOLD, 16));
+        txtdata = new JTextField(30);
+        add(namet);
+        add(Box.createRigidArea(new Dimension(1, 5)));
+        add(typet);
+        add(Box.createRigidArea(new Dimension(1, 5)));
+        add(post);
+        add(Box.createRigidArea(new Dimension(1, 5)));
+        add(label);
+        add(Box.createRigidArea(new Dimension(1, 5)));
+        add(txtdata);
+        add(calbtn);
+        calbtn.addActionListener(this);
+        BoxLayout boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
+        setLayout(boxLayout);
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == calbtn) {
+            String data = txtdata.getText();
+            System.out.println(data);
+        }
     }
 }
