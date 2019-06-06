d7c224103fbd426205a3991386caee7dd9a83230
rayane.amrouche <rayane.amrouche@epita.fr>
2019-06-06 19:35:08 +0200

    [ENGINE][MAP] correction on print

--- src/main/java/MapPannel/MapView.java
+++ src/main/java/MapPannel/MapView.java
@@ -32,7 +32,7 @@ public class MapView extends JPanel implements Observer {
 
     private void drawGrid(Graphics g) {
         Color savedColor = g.getColor();
-        g.setColor(new Color(0,0,0, 200));
+        g.setColor(new Color(0, 0, 0, 200));
         for (int x = 0; x < mapModel.getHeight() * 16; x += 16) {
             g.drawLine(0, x, mapModel.getWidth() * 16, x);
         }
@@ -67,28 +67,28 @@ public class MapView extends JPanel implements Observer {
     public void update(Observable o, Object arg) {
         if (arg.getClass() == Tile.class) {
             switch (SpriteTools.mousePointerState) {
-                case MOVE:
-                    if (mapModel.is_moving)
-                        paintComponent(this.getGraphics(), (Tile) arg);
-                    else
-                        paintComponent(this.getGraphics(), (Tile) arg);
-                    break;
-                default:
+            case MOVE:
+                if (mapModel.is_moving)
                     paintComponent(this.getGraphics(), (Tile) arg);
+                else
+                    paintComponent(this.getGraphics(), (Tile) arg);
+                break;
+            default:
+                paintComponent(this.getGraphics(), (Tile) arg);
             }
         } else if (arg.getClass() == EngineObj.class) {
             switch (SpriteTools.mousePointerState) {
-                case DELETE:
+            case DELETE:
+                deleteEngineOBJ(this.getGraphics(), (EngineObj) arg);
+                break;
+            case MOVE:
+                if (mapModel.is_moving)
                     deleteEngineOBJ(this.getGraphics(), (EngineObj) arg);
-                    break;
-                case MOVE:
-                    if (mapModel.is_moving)
-                        deleteEngineOBJ(this.getGraphics(), (EngineObj) arg);
-                    else
-                        paintComponent(this.getGraphics(), (EngineObj) arg);
-                    break;
-                default:
+                else
                     paintComponent(this.getGraphics(), (EngineObj) arg);
+                break;
+            default:
+                paintComponent(this.getGraphics(), (EngineObj) arg);
             }
         } else if (arg.getClass() == String.class && ((String) arg).equals("toggleGrid")) {
             if (SpriteTools.grid_display)
@@ -115,8 +115,7 @@ public class MapView extends JPanel implements Observer {
             for (int y = y_min; y <= y_max; y++) {
                 Tile tile = mapModel.map.getGameTile(x, y);
                 if (tile != null)
-                    g.drawImage(SpriteTools.pathToImg.get(tile.get_path()),
-                            tile.get_x() * 16, tile.get_y() * 16, null);
+                    g.drawImage(SpriteTools.pathToImg.get(tile.get_path()), tile.get_x() * 16, tile.get_y() * 16, null);
             }
         }
     }
@@ -128,7 +127,8 @@ public class MapView extends JPanel implements Observer {
 
     public void paintComponent(Graphics g, EngineObj obj) {
         BufferedImage img = SpriteTools.openObject(obj.getSprite_());
-        img = img.getSubimage(32, 0, 32, 32);
+        if (obj.isAlive())
+            img = img.getSubimage(32, 0, 32, 32);
         g.drawImage(img, obj.get_x() * 16, obj.get_y() * 16, null);
     }
 
