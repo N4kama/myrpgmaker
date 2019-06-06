2435ea3db72d08c7e38665b45af111a82ffd6d40
Zachary Fakir <zachary.fakir@epita.fr>
2019-06-06 17:52:24 +0200

    [INSPECTOR] InspectorModel reimplemented

--- src/main/java/InspectorPannel/InspectorModel.java
+++ src/main/java/InspectorPannel/InspectorModel.java
@@ -2,18 +2,51 @@ package InspectorPannel;
 
 import Engine.Character.EngineObj;
 
+import javax.swing.*;
 import java.util.Observable;
 import java.util.Observer;
 
-public class InspectorModel implements Observer {
+public class InspectorModel implements Observer   {
     public static EngineObj obj;
-
+    String namet;
+    String typet;
+    String post;
+    String old;
+    JTextField dialog;
     public InspectorModel(EngineObj obj) {
         this.obj = obj;
+        setInspector();
+    }
+
+    void setInspector(){
+        if (obj != null) {
+            namet = "Name: " + obj.getName_();
+
+            if (!obj.getIs_player())
+                typet = "Type: NPC";
+            else
+                typet = "Type: Player";
+            post = "Position: " + obj.getPosition_();
+            if (obj.getDialog() != null)
+                old = "Current dialog: " + obj.getDialog();
+            else
+                old = "Current dialog: ";
+        }
+        else
+        {
+            namet = "Name: ";
+            typet = "Type: ";
+            post = "Position: ";
+            old = "Current dialog: ";
+        }
     }
 
     @Override
     public void update(Observable o, Object arg) {
-
+        obj = (EngineObj) arg;
+        setInspector();
     }
+
+
+
 }
