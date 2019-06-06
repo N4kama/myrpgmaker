f6f9a365c4cd4e223275d360ff657af53142690d
Zachary Fakir <zachary.fakir@epita.fr>
2019-06-06 22:34:25 +0200

    [INSPECTOR] InspectorView fullydone

--- src/main/java/InspectorPannel/InspectorView.java
+++ src/main/java/InspectorPannel/InspectorView.java
@@ -1,10 +1,14 @@
 package InspectorPannel;
 
+import Engine.Character.EngineObj;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
 
 
-public class InspectorView extends JPanel {
+public class InspectorView extends JPanel  implements Observer {
     JLabel namet;
     JLabel typet;
     JLabel post;
@@ -15,7 +19,10 @@ public class InspectorView extends JPanel {
 
     public InspectorView(InspectorModel model) {
         this.model = model;
-        setText();
+        namet = new JLabel(model.namet);
+        typet = new JLabel(model.typet);
+        post = new JLabel(model.post);
+        old = new JLabel(model.old);
 
         JLabel label = new JLabel("Set the dialog parameters:", SwingConstants.LEFT);
         dialog = new JTextField(30);
@@ -34,16 +41,21 @@ public class InspectorView extends JPanel {
         add(calbtn);
         BoxLayout boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
         setLayout(boxLayout);
-
+        model.addObserver(this);
     }
 
     public void setText(){
-        namet = new JLabel(model.namet);
-        typet = new JLabel(model.typet);
-        post = new JLabel(model.post);
-        old = new JLabel(model.old);
+        namet.setText(model.namet);
+        typet.setText(model.typet);
+        post.setText(model.post);
+        old.setText(model.old);
 
     }
 
 
+    @Override
+    public void update(Observable o, Object arg) {
+        setText();
+        this.updateUI();
+    }
 }
