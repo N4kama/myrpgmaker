083c3647457c1694f0270017232d96b9dfac7eaf
Zachary Fakir <zachary.fakir@epita.fr>
2019-06-07 01:23:03 +0200

    [INSPECTOR] Teleport position added to InspectorView

--- src/main/java/InspectorPannel/InspectorView.java
+++ src/main/java/InspectorPannel/InspectorView.java
@@ -13,7 +13,13 @@ public class InspectorView extends JPanel  implements Observer {
     JLabel typet;
     JLabel post;
     JLabel old;
+    JLabel teleport;
+
+    JTextField get_telx;
+    JTextField get_tely;
     JTextField dialog;
+    JButton telbtn = new JButton("Save new teleport position !");
+
     JButton calbtn = new JButton("Save new dialog !");
     public InspectorModel model;
 
@@ -23,6 +29,11 @@ public class InspectorView extends JPanel  implements Observer {
         typet = new JLabel(model.typet);
         post = new JLabel(model.post);
         old = new JLabel(model.old);
+        teleport = new JLabel(model.teleport);
+
+        JLabel tel_t = new JLabel("Set the teleport parameters:");
+        get_telx = new JTextField(3);
+        get_tely = new JTextField(3);
 
         JLabel label = new JLabel("Set the dialog parameters:", SwingConstants.LEFT);
         dialog = new JTextField(30);
@@ -33,8 +44,14 @@ public class InspectorView extends JPanel  implements Observer {
         add(Box.createRigidArea(new Dimension(1, 5)));
         add(post);
         add(Box.createRigidArea(new Dimension(1, 5)));
+        add(teleport);
+        add(Box.createRigidArea(new Dimension(1, 5)));
         add(old);
         add(Box.createRigidArea(new Dimension(1, 5)));
+        add(tel_t);
+        add(get_telx);
+        add(get_tely);
+        add(telbtn);
         add(label);
         add(Box.createRigidArea(new Dimension(1, 5)));
         add(dialog);
@@ -49,7 +66,7 @@ public class InspectorView extends JPanel  implements Observer {
         typet.setText(model.typet);
         post.setText(model.post);
         old.setText(model.old);
-
+        teleport.setText(model.teleport);
     }
 
 
