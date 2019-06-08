7c1f6290361a6ee7c6dfb54d9dee7faaec801913
Zachary Fakir <zachary.fakir@epita.fr>
2019-06-08 19:36:07 +0200

    [EDITOR] EditorEvent implemented

--- src/main/java/Utils/EditorEvent.java
+++ src/main/java/Utils/EditorEvent.java
@@ -5,6 +5,17 @@ import Game.Map;
 
 public class EditorEvent {
 
+    public EditorEvent(EventType action, Map map, int x, int y) {
+        this.action = action;
+        this.map = map;
+        this.x = x;
+        this.y = y;
+        old_path = map.getGameTile(x, y).get_path();
+        old_path_img = map.getGameTile(x, y).getTile_img_();
+        was_walkable =  map.getGameTile(x, y).isIs_Walkable_();
+        object = null;
+    }
+
 
     public EventType action;
     Map map;
