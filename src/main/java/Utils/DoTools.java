339a9ea13d2ebf07c3163d14f41d02955de79628
Zachary Fakir <zachary.fakir@epita.fr>
2019-06-08 19:34:17 +0200

    [EDITOR] undo implemented

--- src/main/java/Utils/DoTools.java
+++ src/main/java/Utils/DoTools.java
@@ -3,6 +3,32 @@ package Utils;
 import java.util.Stack;
 
 public class DoTools {
+    public static void undo() {
+        if (undo_stack.empty())
+        {
+            System.out.println("C'est vide mon gars");
+            return;
+        }
+        EditorEvent event = undo_stack.pop();
+        if (event.action == EditorEvent.EventType.ADD_TILE) {
+            System.out.println("On est bien ici undo");
+            addRedoEvent(new EditorEvent(EditorEvent.EventType.ADD_TILE, event.map, event.x, event.y));
+            event.map.setTile(event.x, event.y, event.old_path);
+            event.map.getGameTile(event.x, event.y).setTile_img_(event.old_path_img);
+            event.map.getGameTile(event.x, event.y).setIs_Walkable(event.was_walkable);
+        }
+        else if (event.action == EditorEvent.EventType.ADD_OBJECT) {
+            event.map.getGameObjects().add(event.object);
+            event.action = EditorEvent.EventType.DEL_OBJECT;
+            addRedoEvent(event);
+        }
+        else if (event.action == EditorEvent.EventType.DEL_OBJECT) {
+            event.map.getGameObjects().add(event.object);
+            event.action = EditorEvent.EventType.ADD_OBJECT;
+            addRedoEvent(event);
+        }
+    }
+
 
 
     public static void addUndoEvent(EditorEvent event) {
