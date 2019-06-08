f4e3f392f18e8d2ee1c36ff1cbe0388bac715056
Zachary Fakir <zachary.fakir@epita.fr>
2019-06-08 19:34:35 +0200

    [EDITOR] redo implemented

--- src/main/java/Utils/DoTools.java
+++ src/main/java/Utils/DoTools.java
@@ -29,7 +29,31 @@ public class DoTools {
         }
     }
 
-
+    public static void redo() {
+        if (redo_stack.empty())
+        {
+            System.out.println("C'est vide mon gars");
+            return;
+        }
+        EditorEvent event = redo_stack.pop();
+        if (event.action.equals(EditorEvent.EventType.ADD_TILE)) {
+            System.out.println("On est bien ici redo");
+            addUndoEvent(new EditorEvent(EditorEvent.EventType.ADD_TILE, event.map, event.x, event.y));
+            event.map.setTile(event.x, event.y, event.old_path);
+            event.map.getGameTile(event.x, event.y).setTile_img_(event.old_path_img);
+            event.map.getGameTile(event.x, event.y).setIs_Walkable(event.was_walkable);
+        }
+        else if (event.action.equals(EditorEvent.EventType.ADD_OBJECT) ) {
+            event.map.getGameObjects().add(event.object);
+            event.action = EditorEvent.EventType.DEL_OBJECT;
+            addUndoEvent(event);
+        }
+        else if (event.action.equals(EditorEvent.EventType.DEL_OBJECT)) {
+            event.map.getGameObjects().add(event.object);
+            event.action = EditorEvent.EventType.ADD_OBJECT;
+            addUndoEvent(event);
+        }
+    }
 
     public static void addUndoEvent(EditorEvent event) {
         undo_stack.push(event);
