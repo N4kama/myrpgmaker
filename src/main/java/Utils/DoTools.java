47589fa6bd8b7e33d340a6c8ed4cabb35db147fc
Zachary Fakir <zachary.fakir@epita.fr>
2019-06-08 19:33:49 +0200

    [EDITOR] Stack func added

--- src/main/java/Utils/DoTools.java
+++ src/main/java/Utils/DoTools.java
@@ -5,6 +5,14 @@ import java.util.Stack;
 public class DoTools {
 
 
+    public static void addUndoEvent(EditorEvent event) {
+        undo_stack.push(event);
+    }
+
+    public static void addRedoEvent(EditorEvent event) {
+        redo_stack.push(event);
+    }
+
      static Stack<EditorEvent> undo_stack = new Stack<>();
      static Stack<EditorEvent> redo_stack = new Stack<>();
 }
