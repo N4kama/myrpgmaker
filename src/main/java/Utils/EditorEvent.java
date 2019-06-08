503ab2c3edf007c07b896f64ca2f412348c4abd0
Zachary Fakir <zachary.fakir@epita.fr>
2019-06-08 19:35:40 +0200

    [EDITOR] EditorEvent class created

--- src/main/java/Utils/EditorEvent.java
+++ src/main/java/Utils/EditorEvent.java
@@ -2,8 +2,16 @@ package Utils;
 
 import Engine.Character.EngineObj;
 import Game.Map;
-import MapPannel.MapModel;
 
 public class EditorEvent {
 
+
+    public EventType action;
+    Map map;
+    public int x;
+    public int y;
+    String old_path;
+    String old_path_img;
+    boolean was_walkable;
+    EngineObj object;
 }
